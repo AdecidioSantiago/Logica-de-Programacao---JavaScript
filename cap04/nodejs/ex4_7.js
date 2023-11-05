6beb2a4b2b49b26233696aca6b99c91ab97e0bc2
AdecidioSantiago <adecidio42@gmail.com>
2023-11-05 10:24:28 -0300

    criate ex4_7 com nodejs

--- cap04/nodejs/ex4_7.js
+++ cap04/nodejs/ex4_7.js
@@ -1,10 +1,10 @@
-const prompt = require("prompt-sync")()
-const pessoas = Number(prompt("Nº de pessoas: "))
+const prompt = require("prompt-sync")()           //adiciona o pacote prompt-sync
+const pessoas = Number(prompt("Nº de pessoas: ")) //lê os dados
 const peixes = Number(prompt("Nº de peixes: "))
-let pagar
-if (peixes > pessoas){
+let pagar                      //declara a variável pagar
+if (pessoas >= peixes){        //condicional sem taxa extra
+    pagar = pessoas *20
+}else{                         //condicional com taxa extra
     pagar = (pessoas * 20) + ((peixes - pessoas) * 12)
-}else {
-    pagar = pessoas * 20
 }
-console.log(`Total a pagar R$: ${pagar.toFixed(2)}`)
+console.log(`Total a pagar: R$ ${pagar.toFixed(2)}`) //exibe o valor total a pagar
