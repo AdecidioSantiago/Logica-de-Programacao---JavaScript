1b14ec133d05dce8d0813f7f107678442e501db1
AdecidioSantiago <adecidio42@gmail.com>
2023-11-05 12:26:00 -0300

    corrigindo var num1

--- cap04/nodejs/ex4_9.js
+++ cap04/nodejs/ex4_9.js
@@ -7,4 +7,4 @@ if (numero < 100 || numero >= 1000){       //condicional caso não seja centena
 const num1 = Math.floor(numero / 100)          //obtém o 1º número
 const num2 = Math.floor((numero % 100) / 10)   //obtém o 2º número
 const num3 = numero % 10                       //obtém o 3º número
-console.log(`Número Invertido: ${num3}${num2}${num3}`)   //exibe o número invertido
+console.log(`Número Invertido: ${num3}${num2}${num1}`)   //exibe o número invertido
