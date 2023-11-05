cddda146753b0aa6706433869006661967a08113
AdecidioSantiago <adecidio42@gmail.com>
2023-11-05 12:18:47 -0300

    create ex4_9 "Invertendo Números"

---
+++ cap04/nodejs/ex4_9.js
@@ -0,0 +1,10 @@
+const prompt = require('prompt-sync')()   //adiciona pacote prompt-sync
+const numero = Number(prompt('Informe uma centena: '))  //lê o número
+if (numero < 100 || numero >= 1000){       //condicional caso não seja centena
+    console.log("Erro... deve ser uma centena")
+    return
+}
+const num1 = Math.floor(numero / 100)          //obtém o 1º número
+const num2 = Math.floor((numero % 100) / 10)   //obtém o 2º número
+const num3 = numero % 10                       //obtém o 3º número
+console.log(`Número Invertido: ${num3}${num2}${num3}`)   //exibe o número invertido
